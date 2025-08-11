3a458ed5f591161476f405207ccfd9365dd0bace
Hasbi Nabil <hasbinabil.work@gmail.com>
2025-08-11 12:56:18 +0700

    style: update new font style

--- app/layout.js
+++ app/layout.js
@@ -1,12 +1,17 @@
-import { Outfit, Ovo } from 'next/font/google';
+import { Lora, Poppins, Source_Code_Pro } from 'next/font/google';
 import './globals.css';
 
-const outfit = Outfit({
+const sc_pro = Source_Code_Pro({
 	subsets: ['latin'],
 	weight: ['400', '500', '600', '700'],
 });
 
-const ovo = Ovo({
+const poppins = Poppins({
+	subsets: ['latin'],
+	weight: ['400'],
+});
+
+const lora = Lora({
 	subsets: ['latin'],
 	weight: ['400'],
 });
@@ -20,7 +25,7 @@ export default function RootLayout({ children }) {
 	return (
 		<html lang="en" className="scroll-smooth">
 			<body
-				className={`${outfit.className} ${ovo.className} antialiased leading-8 overflow-x-hidden`}
+				className={`${sc_pro.className} ${lora.className} antialiased leading-8 overflow-x-hidden`}
 			>
 				{children}
 			</body>
